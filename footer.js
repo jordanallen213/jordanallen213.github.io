b41f059402c46c19f6633e0c2c8a71041454420f
jordanallen213 <jordanallen213@gmail.com>
2024-01-29 09:07:17 -0800

    fixing navbar more

--- footer.js
+++ footer.js
@@ -1,12 +1,33 @@
-const template = document.createElement('template');
+//const template = document.createElement('template');
 
-template.innerHTML = `
-<link href="stylesheet.css" rel="stylesheet">
-<footer>
-<small>
-Copyright &copy; 2024 Jordan Allen. All Rights Reserved.
-</small>
-</footer>
-`;
+//template.innerHTML = `
+//<link href="stylesheet.css" rel="stylesheet">
+//<footer>
+//<small>
+//Copyright &copy; 2024 Jordan Allen. All Rights Reserved.
+//</small>
+//</footer>
+//`;
 
-document.body.appendChild(template.content);
+//document.body.appendChild(template.content); 
+
+
+class customfooter extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+
+    connectedCallback() {
+    this.innerHTML = `
+    <link href="stylesheet.css" rel="stylesheet">
+    <footer>
+    <small>
+    Copyright &copy; 2024 Jordan Allen. All Rights Reserved.
+    </small>
+    </footer>
+    `;
+    }
+}
+
+  customElements.define('footer-component', customfooter);
