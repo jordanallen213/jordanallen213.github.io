e3f00d3eb9059d5bb2611e40a237ffcd7b16555c
jordanallen213 <jordanallen213@gmail.com>
2024-01-31 12:41:30 -0800

    clickable header

--- header.js
+++ header.js
@@ -8,8 +8,10 @@ class Header extends HTMLElement {
     this.innerHTML = `
     <link href="stylesheet.css" rel="stylesheet">
       <header>
+      <a href="https://jordanallen213.github.io/">
     <img src="https://lh3.googleusercontent.com/pw/ABLVV86b6jxXS45SCYu5zCRQTGGbNh20_SmgzW6bnXXQ0s-E0iHFfm7gV9JxbhOFKLSed2QTWByay1skwwNcuSPrCqyhHx5mEo7ntfOMTpAHX1wWZiOIlkOB=w1920-h1080" style="width: 90%;">
-      </header>
+    </a>  
+    </header>
     `;
     }
 }
