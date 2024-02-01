5ac04620a5e515f9048e41c106b1cb403ac14280
jordanallen213 <jordanallen213@gmail.com>
2024-02-01 11:30:59 -0800

    contact

--- nav.js
+++ nav.js
@@ -7,7 +7,7 @@ template.innerHTML = `
 <li><a href="https://jordanallen213.github.io/pages/climbingtrips/climbingtriphome.html">Climbing Trips</a></li>
 <li><a href="https://jordanallen213.github.io/pages/othertripreports/othertripreports.html">Other Trip Reports</a></li>
 <li><a href="https://jordanallen213.github.io/pages/forsale/forsale.html">Gear For Sale</a></li>
-<li style="float:right"><a class="active" href="https://jordanallen213.github.io/pages/contact.html">Contact Me</a></li>
+<li style="float:right"><a class="active" href="https://jordanallen213.github.io/pages/contactme/contact.html">Contact Me</a></li>
 </ul>
 `;
 
