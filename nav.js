c8638af9a96968f4e48d12fba9754d2d4f4382a4
jordanallen213 <jordanallen213@gmail.com>
2024-01-31 20:45:06 -0800

    fixing nav

--- nav.js
+++ nav.js
@@ -4,7 +4,7 @@ template.innerHTML = `
 <link href="stylesheet.css" rel="stylesheet">
 <ul>
 <li><a class="active" href="https://jordanallen213.github.io/">Home</a></li>
-<li><a href="https://jordanallen213.github.io/pages/bydate.html">Climbing Trips</a></li>
+<li><a href="https://jordanallen213.github.io/pages/climbingtrips/climbingtriphome.html">Climbing Trips</a></li>
 <li><a href="https://jordanallen213.github.io/pages/othertripreports/othertripreports.html">Other Trip Reports</a></li>
 <li><a href="https://jordanallen213.github.io/pages/forsale/forsale.html">Gear For Sale</a></li>
 <li style="float:right"><a class="active" href="https://jordanallen213.github.io/pages/contact.html">Contact Me</a></li>
