c46d234aba84faf5be8ed11a78cec5cdc8626372
jordanallen213 <jordanallen213@gmail.com>
2024-01-31 18:12:39 -0800

    updatig everything

--- nav.js
+++ nav.js
@@ -5,9 +5,8 @@ template.innerHTML = `
 <ul>
 <li><a class="active" href="https://jordanallen213.github.io/">Home</a></li>
 <li><a href="https://jordanallen213.github.io/pages/bydate.html">Climbing Trips</a></li>
-<li><a href="https://jordanallen213.github.io/pages/backpacking.html">Backpacking Trips</a></li>
-<li><a href="https://jordanallen213.github.io/pages/rivertrips.html">River Trips</a></li>
-<li><a href="https://jordanallen213.github.io/pages/forsale.html">For Sale</a></li>
+<li><a href="https://jordanallen213.github.io/pages/othertripreports/othertripreports.html">Other Trip Reports</a></li>
+<li><a href="https://jordanallen213.github.io/pages/forsale/forsale.html">Gear For Sale</a></li>
 <li style="float:right"><a class="active" href="https://jordanallen213.github.io/pages/contact.html">Contact Me</a></li>
 </ul>
 `;
