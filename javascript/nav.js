6abb743b138d3ef0027602c7dcadefa61c762e08
jordanallen213 <jordanallen213@gmail.com>
2024-02-02 12:45:27 -0800

    Update nav.js

--- javascript/nav.js
+++ javascript/nav.js
@@ -3,11 +3,11 @@ const template = document.createElement('template');
 template.innerHTML = `
 <link href="stylesheet.css" rel="stylesheet">
 <ul>
-<li><a class="active" href="https://jordanallen213.github.io/">Home</a></li>
-<li><a href="https://jordanallen213.github.io/pages/climbingtrips/climbingtriphome.html">Climbing Trips</a></li>
-<li><a href="https://jordanallen213.github.io/pages/othertripreports/othertripreports.html">Other Trip Reports</a></li>
-<li><a href="https://jordanallen213.github.io/pages/forsale/forsale.html">Gear For Sale</a></li>
-<li style="float:right"><a class="active" href="https://jordanallen213.github.io/pages/contactme/contact.html">Contact Me</a></li>
+<li><a class="active" href="https://altitudeaddict.com/">Home</a></li>
+<li><a href="https://altitudeaddict.com/pages/climbingtrips/climbingtriphome.html">Climbing Trips</a></li>
+<li><a href="https://altitudeaddict.com/pages/othertripreports/othertripreports.html">Other Trip Reports</a></li>
+<li><a href="https://altitudeaddict.com/pages/forsale/forsale.html">Gear For Sale</a></li>
+<li style="float:right"><a class="active" href="https://altitudeaddict.com/pages/contactme/contact.html">Contact Me</a></li>
 </ul>
 `;
 
