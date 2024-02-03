8f9a10cafb218b74be6e17a50ffade43f37cd8c3
jordanallen213 <jordanallen213@gmail.com>
2024-02-03 11:12:33 -0800

    deletign

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,25 +1,4 @@
-//class customfooter extends HTMLElement {
-  //  constructor() {
-    //  super();
-   // }
-  
-
-    //connectedCallback() {
-//    this.innerHTML = `
-  //  <link href="stylesheet.css" rel="stylesheet">
-    //<footer>
-    //<small>
-    //Copyright &copy; 2020-2024 Jordan Allen. All Rights Reserved.
-    //<a href="https://altitudeaddict.com/pages/contactme/contact.html" style="text-decoration:none; color: black;"><u>Contact</u></a>
-    //</small>
-    //</footer>
-    //`;
-   // }
-//}
-
- // customElements.define('footer-component', customfooter);
-
- const footerTemplate = document.createElement('template');
+const footerTemplate = document.createElement('template');
 
  footerTemplate.innerHTML = `
      <link href="stylesheet.css" rel="stylesheet">
