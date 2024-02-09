4fafb94d77fe5068f367815ff8b9262e7eb9d64d
jordanallen213 <jordanallen213@gmail.com>
2024-02-09 10:55:26 -0800

    seo meta stuff

--- javascript/headfootnav.js
+++ javascript/headfootnav.js
@@ -1,9 +1,8 @@
 const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML = `
-<link href="css/stylesheet.css" rel="stylesheet">
 <header>
 <a href="https://altitudeaddict.com/">
-<img src="https://lh3.googleusercontent.com/pw/ABLVV86_pdC4kgBiA1VKXO8fQwCQH45JvPTnMXPzpOmbOaAPhkEFu7_Ey1WXKJ1uEu_TekJwn_XAUpKoSm8hRzCdoRC0Cid5oqtnPXlS9ylju4cRRo12voM6=w1920-h1080" style="width: 90%;">
+<img src="https://lh3.googleusercontent.com/pw/ABLVV86_pdC4kgBiA1VKXO8fQwCQH45JvPTnMXPzpOmbOaAPhkEFu7_Ey1WXKJ1uEu_TekJwn_XAUpKoSm8hRzCdoRC0Cid5oqtnPXlS9ylju4cRRo12voM6=w1920-h1080" style="width: 90%;" alt="peak 6148 alaska seen from peak 5750">
 </a>  
 </header>
 `;
@@ -11,7 +10,6 @@ document.body.prepend(headerTemplate.content);
 
 const navTemplate = document.createElement('template');
 navTemplate.innerHTML = `
-<link href="stylesheet.css" rel="stylesheet">
 <ul>
 <li><a class="active" href="https://altitudeaddict.com/">Home</a></li>
 <li><a href="https://altitudeaddict.com/pages/climbingtrips/climbingtriphome.html">Climbing Trips</a></li>
@@ -24,7 +22,6 @@ document.querySelector('header').appendChild(navTemplate.content);
 
 const footerTemplate = document.createElement('template');
  footerTemplate.innerHTML = `
-     <link href="stylesheet.css" rel="stylesheet">
      <footer>
      <small>
      Copyright &copy; 2020-2024 Jordan Allen. All Rights Reserved.
