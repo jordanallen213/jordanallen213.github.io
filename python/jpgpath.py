108af60b470c4be890d85db91cee3ca97066caa7
jordanallen213 <jordanallen213@gmail.com>
2024-02-20 22:17:56 -0800

    Update jpgpath.py

--- python/jpgpath.py
+++ python/jpgpath.py
@@ -1,7 +1,7 @@
 import os
 
 # fill this out
-directory_path = '../includes/photos/climbingphotos/eaglelake2023'
+directory_path = '../includes/photos/othertripphotos/denalideathmarch2023'
 output_file_path = 'output.txt'
 
 
