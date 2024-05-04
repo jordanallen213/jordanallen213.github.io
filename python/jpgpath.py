7ce39cd775f65c8e604361b1b1ea15b46dc916ee
jordanallen213 <jordanallen213@gmail.com>
2024-05-04 13:24:47 -0700

    adding casaval

--- python/jpgpath.py
+++ python/jpgpath.py
@@ -1,7 +1,7 @@
 import os
 
 # fill this out
-directory_path = '../includes/photos/othertripphotos/denalibikepacking2023'
+directory_path = '../includes/photos/climbingphotos/casaval2024'
 output_file_path = 'output.txt'
 
 
