13f791922abef280aa4f7efed1c2316140297466
jordanallen213 <jordanallen213@gmail.com>
2024-05-29 15:22:15 -0700

    re adding marmo photos

--- python/jpgpath.py
+++ python/jpgpath.py
@@ -1,7 +1,7 @@
 import os
 
 # fill this out
-directory_path = '../includes/photos/climbingphotos/roundtop2023'
+directory_path = '../includes/photos/climbingphotos/marmogeddon2024'
 output_file_path = 'output.txt'
 
 
