fc552566a59933caad6de7a460b2eafb6225ad71
jordanallen213 <jordanallen213@gmail.com>
2024-05-30 16:13:58 -0700

    taking out lazy loading

--- python/jpgpath.py
+++ python/jpgpath.py
@@ -1,7 +1,7 @@
 import os
 
 # fill this out
-directory_path = '../includes/photos/climbingphotos/marmogeddon2024'
+directory_path = '../includes/photos/climbingphotos/casaval2024'
 output_file_path = 'output.txt'
 
 
@@ -16,7 +16,7 @@ def write_paths_to_file(image_paths, output_file):
     with open(output_file, 'w') as file:
         for path in image_paths:
             path = path.replace('.JPG', '.jpg')
-            file.write(f'<img src="../../'+ path + '" loading="lazy">'+'\n')
+            file.write(f'<img src="../../'+ path + '">'+'\n')
 
 image_paths = get_image_paths(directory_path)
 write_paths_to_file(image_paths, output_file_path)
