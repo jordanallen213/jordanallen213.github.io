e7e01c130a5d5151485c30556303086258b1f99e
jordanallen213 <jordanallen213@gmail.com>
2024-10-09 16:35:48 -0700

    carlo ski

--- python/jpgpath.py
+++ python/jpgpath.py
@@ -1,7 +1,7 @@
 import os
 
 # fill this out
-directory_path = '../includes/photos/othertripphotos/denalideathmarch2023'
+directory_path = '../includes/photos/skitripphotos/carloski2024'
 output_file_path = 'output.txt'
 
 
